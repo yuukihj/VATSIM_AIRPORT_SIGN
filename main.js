a61fa138f815a7e5a3aea937889fed0a99185634
HyunJae Lee <yuuki_hj@naver.com>
2024-10-23 21:16:09 +0900

    main.js edited
    main.js edited

---
+++ main.js
@@ -0,0 +1,29 @@
+// main.js
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'), // 필요하면 추가
+    },
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'resources', 'index.html'));
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
