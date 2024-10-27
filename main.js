27fa93af07fe3e4becacd9e242460dbf257337b4
HyunJae Lee <yuuki_hj@naver.com>
2024-10-27 22:50:05 +0900

    window size changed

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const path = require('path');
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'), // 필요하면 추가
     },
